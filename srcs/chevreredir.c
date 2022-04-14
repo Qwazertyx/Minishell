01ebd7f666634092a26f635257048faa522c3fe5
Victor Sedat <vsedat@z4r3p5.42lyon.fr>
2022-04-14 15:12:52 +0200

    << is ~ok~, need parsing

--- srcs/chevreredir.c
+++ srcs/chevreredir.c
@@ -1,14 +1,30 @@
 #include "../incl/minishell.h"
 
+int	whoislastdouble(char **file)
+{
+	int	i;
+	int	y;
+
+	i = 0;
+	y = 0;
+	while (file[i] && file[i][1])
+	{
+		if (file[i][0] == '<')
+			y = i;
+		i++;
+	}
+	return (y);
+}
+
 char	*doublechevre(char **file, t_var *tab, int i)
 {
 	int		y;
-	char	*heredoc;
 	char	*stop;
 	char	*read;
+	char	**heredoc;
 
 	y = 0;
-	stop = malloc(ft_strlen(file[i]) - 2 + 1);
+	stop = malloc((ft_strlen(file[i]) - 2 + 1) * sizeof(char *));
 	while (file[i][y + 2])
 	{
 		stop[y] = file[i][y + 2];
@@ -16,11 +32,15 @@ char	*doublechevre(char **file, t_var *tab, int i)
 	}
 	stop[y] = 0;
 	read = readline(0);
-	while (!ft_strnstr(read, stop, ft_strlen(read)))
+	if (i == whoislastdouble(file))
+		ft_putstr_fd(read, 3);
+	while (ft_strcmp(read, stop))
 	{
 		read = readline(0);
+		if (i == whoislastdouble(file))
+			ft_putstr_fd(read, 3);
 	}
-	dprintf(2, "sortie sortie sortie\n");
+	free(stop);
 	return (read);
 }
 
@@ -29,33 +49,25 @@ int	ft_chevreg(char **file, t_var *tab)
 	int		i;
 	int		fd;
 
+	dprintf(2, "last: %d\n == %s\n", whoislastdouble(file), file[whoislastdouble(file)]);
 	i = 0;
 	while (file[i])
 	{
 		if (file[i][0] != '<')
 		{
-			dprintf(2, "sheeesh : %s\n", file[i]);
 			fd = open(&file[i][2], O_RDONLY);
 			close(fd);
 		}
 		else if (file[i][0] == '<')
-		{
-			dprintf(2, "double sheeesh : %s\n", file[i]);
 			doublechevre(file, tab, i);
-		}
 		i++;
 	}
-	if (file[i - 1][2] != '<')
+	if (file[i - 1][0] != '<')
 	{
 		fd = open(&file[i - 1][2], O_RDONLY);
 		if (fd < 0)
 			terminate();
 		dup2(fd, STDIN_FILENO);
-	}
-	else
-	{
-		doublechevre(file, tab, i);
-		dup2(STDOUT_FILENO, STDIN_FILENO);
 	}
 	return (0);
 }
